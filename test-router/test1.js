f31d3193f54a4591265a3f8907730f7735de52c8
qianlongo <1162633511@qq.com>
2018-05-02 23:44:11 +0800

    update: prototype注册原型方法解析

--- test-router/test1.js
+++ test-router/test1.js
@@ -10,6 +10,14 @@ router.get('/', (ctx, next) => {
   ctx.body = 'hello word'
 })
 
+router.use((ctx, next) => {
+  let { path } = ctx
+  console.log(path)
+  if (path === 'use') {
+    ctx.body = 'use'
+  }
+})
+
 app.use(router.routes())
 app.use(router.allowedMethods())
 
