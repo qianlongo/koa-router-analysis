4394524d3f70f768a364884a50109c092db125d0
qianlongo <1162633511@qq.com>
2018-05-06 20:09:20 +0800

    update: 测试path-to-regexp

--- examples/test-path-to-regexp.js
+++ examples/test-path-to-regexp.js
@@ -1,2 +1,17 @@
 const pathToRegexp = require('path-to-regexp')
 
+{
+  let keys = []
+  let re = pathToRegexp('/foo/:bar', keys)
+  // console.log(keys)
+  // console.log(re)
+}
+
+{
+  let keys = []
+  let re = pathToRegexp('/foo/*', keys)
+  console.log(re)
+  console.log(re.test('/foo'))
+  console.log(re.test('/foo/'))
+  console.log(re.test('/foo/a'))
+}
