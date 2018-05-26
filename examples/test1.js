2688d1057dbc51b531d282f722bc8dbc63775dd9
qianlongo <1162633511@qq.com>
2018-05-26 18:45:02 +0800

    update: Layer

--- examples/test1.js
+++ examples/test1.js
@@ -6,26 +6,26 @@ const PORT = 3000
 const app = new Koa()
 const router = new Router()
 
-router.get('/*', (ctx, next) => {
-  ctx.body = 'hello word'
-  next()
-})
+// router.get('/*', (ctx, next) => {
+//   ctx.body = 'hello word'
+//   next()
+// })
 
-router.get('hello', '/hello', (ctx, next) => {
-  ctx.body = 'hello word /hello'
-  console.log('hello word', ctx._matchedRoute, ctx._matchedRouteName)
-  next()
-})
+// router.get('hello', '/hello', (ctx, next) => {
+//   ctx.body = 'hello word /hello'
+//   console.log('hello word', ctx._matchedRoute, ctx._matchedRouteName)
+//   next()
+// })
 
-router.register('/user', [ 'get' ], (ctx, next) => {
-  ctx.body = 'user id'
-  console.log('user', ctx.params.id)
-  next()
-})
+// router.register('/user', [ 'get' ], (ctx, next) => {
+//   ctx.body = 'user id'
+//   console.log('user', ctx.params.id)
+//   next()
+// })
 
 router.register('/user/:id', [ 'get' ], (ctx, next) => {
   ctx.body = 'user'
-  console.log('user')
+  console.log('user', ctx.params)
   next()
 })
 
