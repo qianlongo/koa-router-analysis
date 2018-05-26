aac37c8ebf6f696ecfa23ac6be195a1ea745ee03
qianlongo <1162633511@qq.com>
2018-05-26 18:11:51 +0800

    update: Layer解析

--- examples/test1.js
+++ examples/test1.js
@@ -8,20 +8,31 @@ const router = new Router()
 
 router.get('/*', (ctx, next) => {
   ctx.body = 'hello word'
+  next()
+})
+
+router.get('hello', '/hello', (ctx, next) => {
+  ctx.body = 'hello word /hello'
   console.log('hello word', ctx._matchedRoute, ctx._matchedRouteName)
   next()
 })
 
 router.register('/user', [ 'get' ], (ctx, next) => {
+  ctx.body = 'user id'
+  console.log('user', ctx.params.id)
+  next()
+})
+
+router.register('/user/:id', [ 'get' ], (ctx, next) => {
   ctx.body = 'user'
   console.log('user')
   next()
 })
 
-router.use((ctx, next) => {
-  let { path } = ctx
-  console.log(path)
-})
+// router.use((ctx, next) => {
+//   let { path } = ctx
+//   console.log(path)
+// })
 
 app.use(router.routes())
 app.use(router.allowedMethods())
